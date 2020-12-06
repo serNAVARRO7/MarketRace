08fb590376e56517fb0a9f3b871b883b9449a95a
Sergio Navarro <33727996+serNAVARRO7@users.noreply.github.com>
2020-12-06 14:48:10 +0100

    Added sound effects

---
+++ MarketRace/Assets/Scripts/musica.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musica : MonoBehaviour
+{
+    AudioSource fuenteAudio;
+    public AudioClip cashRegister;
+    public AudioClip silbato;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuenteAudio = GetComponent<AudioSource>();
+        //DontDestroyOnLoad(this.gameObject);
+        //fuenteAudio.clip = cashRegister;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void cashRegisterSound()
+    {
+        fuenteAudio.clip = cashRegister;
+        fuenteAudio.Play();
+    }
+
+    public void silbatoSound()
+    {
+        fuenteAudio.clip = silbato;
+        fuenteAudio.Play();
+    }
+}
